913f5227e03ec94dd595be84271802fe1e62d43c
Subham Karmakar <rikk4335@gmail.com>
2026-01-16 20:39:34 +0530

    added registration page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Register from "./register/page";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <>
+      <Register />
+    </>
   );
 }
---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+export default function Register() {
+  return (
+    <>
+      <div className="bg-zinc-950">
+        <h1>Register Page</h1>
+      </div>
+    </>
+  );
+}
